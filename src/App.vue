936a6f9e961fe1d72d2a40a2b1fcc060f3b3a58a
wdw000 <dowonwang01@gmail.com>
2022-03-23 19:29:00 +0900

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header-nav></header-nav>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderNav from "./components/HeaderNav.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { HeaderNav },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header-nav-wrap">
+    <div class="header-nav">
+      <h1>Wang DoWon</h1>
+      <ul>
+        <li>About Me</li>
+        <li>Skills</li>
+        <li>Archiving</li>
+        <li>Projects</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header-nav-wrap {
+  width: inherit;
+  height: fit-content;
+  background-color: #fff;
+}
+
+.header-nav {
+  width: inherit;
+  height: fit-content;
+  max-width: 1920px;
+  font-size: 1.6rem;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+}
+
+.header-nav-wrap,
+.header-nav {
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+
+.header-nav > h1 {
+  margin: 1em;
+}
+
+.header-nav > ul {
+  display: flex;
+  align-items: center;
+  margin: 1em;
+  width: 40%;
+}
+
+.header-nav > ul > li {
+  flex: 1;
+  text-align: center;
+}
+</style>
