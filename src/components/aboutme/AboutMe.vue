acd4c98d34f12e0ed805f3a7274b45982f3d08bb
wdw000 <dowonwang01@gmail.com>
2022-03-23 22:58:27 +0900

    add aboutme

---
+++ src/components/aboutme/AboutMe.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>About Me</h2>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      <about-me-item :item="item"></about-me-item>
+    </li>
+  </ul>
+</template>
+
+<script>
+import AboutMeItem from "./AboutMeItem.vue";
+export default {
+  components: { AboutMeItem },
+  data() {
+    return {
+      items: [
+        { name: "이름", value: "왕도원" },
+        { name: "생년월일", value: "98.02.20" },
+        { name: "이메일", value: "dowonwang01@gmail.com" },
+        { name: "학력", value: "동양미래대학교(컴퓨터정보공학과)" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/aboutme/AboutMeItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-me-item">
+    <dt>{{ item.name }}</dt>
+    <dd>{{ item.value }}</dd>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: { type: Object, required: true },
+  },
+};
+</script>
+
+<style>
+</style>
