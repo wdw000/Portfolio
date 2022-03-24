25c89846e407b8a8617b869bc9e231399a03ef17
wdw000 <dowonwang01@gmail.com>
2022-03-24 17:36:07 +0900

    add link

---
+++ src/components/link/LinkBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="link-box">
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <link-item :item="item"></link-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import LinkItem from "./LinkItem.vue";
+export default {
+  components: { LinkItem },
+  data() {
+    return {
+      items: [
+        {
+          title: "github",
+          url: "github.com/wdw000",
+          contents: [
+            "공부하면서 끄적였던 소스들",
+            "각종 튜토리얼들",
+            "프로젝트들",
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/link/LinkItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="link-item">
+    <dl>
+      <dt>{{ item.title }}</dt>
+      <dd>{{ item.url }}</dd>
+      <dd v-for="(value, index) in item.contents" :key="index">{{ value }}</dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: { type: Object, required: true },
+  },
+};
+</script>
+
+<style>
+</style>
