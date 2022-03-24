800c586b4e0ff83b04bd4a618a6a55873343a996
wdw000 <dowonwang01@gmail.com>
2022-03-24 16:44:53 +0900

    add skills

---
+++ src/components/skills/SkillsBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="skills">
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        <skills-item :item="item"></skills-item>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import SkillsItem from "./SkillsItem.vue";
+
+export default {
+  components: { SkillsItem },
+  data() {
+    return {
+      items: [
+        {
+          title: "Frontend",
+          titleItem: ["HTML5", "CSS3", "JavaScript", "Vue.js"],
+        },
+        { title: "Backend", titleItem: ["Node.js"] },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/skills/SkillsItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="skills-item">
+    <p>{{ item.title }}</p>
+    <hr />
+    <ul>
+      <li v-for="(value, index) in item.titleItem" :key="index">
+        {{ value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: { type: Object, required: true },
+  },
+};
+</script>
+
+<style>
+</style>
