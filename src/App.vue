a1550a3dc21c94d068d992a7b53137bd4f6504e9
wdw000 <dowonwang01@gmail.com>
2022-03-24 16:44:18 +0900

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <header-nav></header-nav>
   <about-me></about-me>
+  <skills-box></skills-box>
 </template>
 
 <script>
 import AboutMe from "./components/aboutme/AboutMe.vue";
 import HeaderNav from "./components/HeaderNav.vue";
+import SkillsBox from "./components/skills/SkillsBox.vue";
+
 export default {
   name: "App",
-  components: { HeaderNav, AboutMe },
+  components: { HeaderNav, AboutMe, SkillsBox },
 };
 </script>
 
