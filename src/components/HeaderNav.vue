cac79759ab8e9a9b8cc025e6495d0ac860164fcb
wdw000 <dowonwang01@gmail.com>
2022-03-28 13:25:18 +0900

    add small header

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -8,44 +8,109 @@
       <li>Projects</li>
     </ul>
   </nav>
+
+  <nav class="header-nav-small" ref="menu_list">
+    <div>
+      <h1>Wang DoWon</h1>
+      <button @click="openMenu">menu</button>
+    </div>
+
+    <ul v-show="menu">
+      <li>About Me</li>
+      <li>Skills</li>
+      <li>Link</li>
+      <li>Projects</li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      menu: false,
+    };
+  },
+
+  methods: {
+    openMenu() {
+      this.menu = !this.menu;
+    },
+  },
+};
 </script>
 
 <style scoped>
-.header-nav {
-  width: inherit;
-  height: fit-content;
-  max-width: 1300px;
-  display: flex;
-  justify-content: space-between;
-  margin: 0 auto;
-  font-size: 2rem;
-}
-
 .header-nav-wrap,
-.header-nav {
+.header-nav,
+.header-nav-small {
   position: sticky;
   top: 0;
   left: 0;
   z-index: 100;
 }
 
+.header-nav {
+  width: inherit;
+  height: fit-content;
+  max-width: 1300px;
+  display: none;
+  justify-content: space-between;
+  margin: 0 auto;
+  font-size: 2rem;
+  border: solid black 1px;
+  padding: 2rem;
+}
+
 .header-nav > h1 {
-  margin: 1em;
+  flex: 1;
 }
 
 .header-nav > ul {
   display: flex;
   align-items: center;
-  margin: 1em;
-  width: 40%;
+  flex: 1;
 }
 
 .header-nav > ul > li {
   flex: 1;
-  text-align: center;
+  text-align: right;
+  border: solid black 1px;
+}
+
+.header-nav-small {
+  width: inherit;
+  max-width: 1300px;
+  height: fit-content;
+  margin: 0 auto;
+  font-size: 2rem;
+  border: solid black 1px;
+}
+
+.header-nav-small > div {
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem;
+}
+
+.header-nav-small > ul {
+  position: absolute;
+  width: 100%;
+  border: solid 1px black;
+  background-color: #fff;
+  padding: 2rem;
+}
+
+.header-nav-small > ul > li {
+  margin: 1rem 0;
+}
+
+@media (min-width: 719px) {
+  .header-nav-small {
+    display: none;
+  }
+  .header-nav {
+    display: flex;
+  }
 }
 </style>
