67c96027bf26174d5ab5effd238d202770319b86
wdw000 <dowonwang01@gmail.com>
2022-04-01 17:24:15 +0900

    init

---
+++ src/components/HomeBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="Home" class="home-box"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+  height: 100vh;
+  background-color: cyan;
+}
+
+div::before {
+  content: "";
+  display: block;
+  margin-top: -64px;
+  height: 64px;
+}
+</style>
