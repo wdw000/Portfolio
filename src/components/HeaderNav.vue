c0836e69862f2b00586f37565f02107c2e23761e
wdw000 <dowonwang01@gmail.com>
2022-04-01 17:22:21 +0900

    add transition

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,29 +1,60 @@
 <template>
-  <nav class="header-nav" ref="headerNav" :class="{'header-scroll':isScroll}">
-    <h1>Wang DoWon</h1>
-    <ul>
-      <li>About Me</li>
-      <li>Skills</li>
-      <li>Link</li>
-      <li>Projects</li>
-    </ul>
-  </nav>
+  <div :class="['header-nav-wrap', { 'header-scroll': isScroll }]">
+    <nav
+      class="header-nav"
+      ref="headerNav"
+      :class="{ 'header-scroll': isScroll }"
+    >
+      <h1><a href="#Home">Wang DoWon</a></h1>
+      <ul>
+        <li>
+          <a href="#AboutMe"
+            ><div class="underline"></div>
+            About Me</a
+          >
+        </li>
+        <li>
+          <a href="#Skills"
+            ><div class="underline"></div>
+            Skills</a
+          >
+        </li>
+        <li>
+          <a href="#Link"
+            ><div class="underline"></div>
+            Link</a
+          >
+        </li>
+        <li>
+          <a href="#Projects"
+            ><div class="underline"></div>
+            Projects</a
+          >
+        </li>
+      </ul>
+    </nav>
+  </div>
 
-  <nav class="header-nav-small" ref="headerNavSmall" :class="{'header-scroll':isScroll}">
+  <nav
+    class="header-nav-small"
+    ref="headerNavSmall"
+    :class="{ 'header-scroll': isScroll || menu }"
+  >
     <div>
-      <h1>Wang DoWon</h1>
+      <h1><a href="#Home">Wang DoWon</a></h1>
       <button @click="openMenu"></button>
     </div>
-    <transition name="slide" appear>
-      <ul v-if="menu" key="sample-key">
-      <li>About Me</li>
-      <li>Skills</li>
-      <li>Link</li>
-      <li>Projects</li>
-    </ul>
+    <transition name="slide">
+      <ul v-if="menu">
+        <li><a href="#AboutMe" @click="openMenu">About Me</a></li>
+        <li><a href="#Skills" @click="openMenu">Skills</a></li>
+        <li><a href="#Link" @click="openMenu">Link</a></li>
+        <li><a href="#Projects" @click="openMenu">Projects</a></li>
+      </ul>
     </transition>
-    
   </nav>
+
+  <div class="graybox" v-if="menu"></div>
 </template>
 
 <script>
@@ -32,10 +63,12 @@ export default {
     return {
       menu: false,
       isScroll: false,
+      currentView: "Home",
+      ulWidth: 0,
     };
   },
-  mounted () {
-    window.addEventListener('scroll', this.onScroll)
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
   },
   methods: {
     openMenu() {
@@ -43,36 +76,97 @@ export default {
     },
 
     onScroll() {
-      const currentScrollPosition = window.scrollY || document.documentElement.scrollTop;
+      const currentScrollPosition =
+        window.scrollY || document.documentElement.scrollTop;
+      const headerHeight = 65;
 
-      if(currentScrollPosition > 0) {
+      const homeHeight =
+        document.querySelector(".home-box").offsetHeight - headerHeight;
+      const aboutMeHeight =
+        document.querySelector(".about-me").offsetHeight +
+        homeHeight -
+        headerHeight;
+      const skillsHeight =
+        document.querySelector(".skills-box").offsetHeight +
+        aboutMeHeight -
+        headerHeight;
+      const linkHeight =
+        document.querySelector(".link-box").offsetHeight +
+        skillsHeight -
+        headerHeight;
+
+      if (currentScrollPosition > 0) {
         this.isScroll = true;
       } else {
         this.isScroll = false;
       }
-      
-    }
+
+      if (0 <= currentScrollPosition && currentScrollPosition < homeHeight) {
+        this.currentView = "Home";
+      } else if (
+        homeHeight <= currentScrollPosition &&
+        currentScrollPosition < aboutMeHeight
+      ) {
+        this.currentView = "About Me";
+      } else if (
+        aboutMeHeight <= currentScrollPosition &&
+        currentScrollPosition < skillsHeight
+      ) {
+        this.currentView = "Skills";
+      } else if (
+        skillsHeight <= currentScrollPosition &&
+        currentScrollPosition < linkHeight
+      ) {
+        this.currentView = "Link";
+      }
+    },
   },
 };
 </script>
 
 <style scoped>
-.slide-enter-active, .slide-leave-active {
-  transition: 0.5s ease-in;
+/* transition */
+
+.slide-enter-active {
+  -moz-transition-duration: 0.3s;
+  -webkit-transition-duration: 0.3s;
+  -o-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -moz-transition-timing-function: ease-in;
+  -webkit-transition-timing-function: ease-in;
+  -o-transition-timing-function: ease-in;
+  transition-timing-function: ease-in;
 }
 
-.slide-enter-from { 
-  height: 0;
+.slide-leave-active {
+  -moz-transition-duration: 0.3s;
+  -webkit-transition-duration: 0.3s;
+  -o-transition-duration: 0.3s;
+  transition-duration: 0.3s;
+  -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
 }
 
-.slide-enter-to{
-  height: fit-content;
+.slide-enter-from,
+.slide-leave-to {
+  overflow: hidden;
+  max-height: 0;
 }
 
+.slide-enter-to,
+.slide-leave-from {
+  overflow: hidden;
+  max-height: 220px;
+}
+
+/* header */
 .header-nav-wrap,
 .header-nav,
 .header-nav-small {
   position: sticky;
+  height: 64px;
   top: 0;
   left: 0;
   z-index: 100;
@@ -84,7 +178,6 @@ export default {
 
 .header-nav {
   width: inherit;
-  height: fit-content;
   max-width: 1300px;
   display: none;
   justify-content: space-between;
@@ -101,7 +194,6 @@ export default {
   display: flex;
   align-items: center;
   flex: 1;
-  transition: ;
 }
 
 .header-nav > ul > li {
@@ -116,7 +208,6 @@ export default {
   margin: 0 auto;
   font-size: 2rem;
   padding: 2rem;
-
 }
 
 .header-nav-small > div {
@@ -128,21 +219,70 @@ export default {
   position: absolute;
   width: 100%;
   left: 0;
+  top: 64px;
   background-color: #fff;
-  padding: 2rem;
+  border-top: solid lightgray 1px;
+  box-shadow: 0px 5px 5px gray;
 }
 
 .header-nav-small > ul > li {
-  margin: 2rem 0;
+  margin: 2rem;
 }
 
 .header-nav-small > div > button {
   width: 24px;
   height: 24px;
-  background-image: url('@/assets/img/round_menu_black_24dp.png');
+  background-image: url("@/assets/img/round_menu_black_24dp.png");
   background-position: center;
   background-size: contain;
   background-repeat: no-repeat;
+}
+
+a:link,
+a:visited {
+  color: black;
+  text-decoration: none;
+}
+
+li > a {
+  position: relative;
+}
+
+.underline {
+  position: absolute;
+  bottom: -1rem;
+  left: 50%;
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+  border-radius: 10rem;
+  transition: width 0.3s ease 0s, left 0.3s ease 0s;
+  width: 0;
+}
+
+a:hover > .underline {
+  width: 100%;
+  left: 0;
+}
+
+.header-nav > ul > li > a:hover {
+  color: gray;
+}
+
+.spread-underline:hover:after {
+  width: 100%;
+  left: 0;
+}
+
+.graybox {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: gray;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1;
+  opacity: 0.5;
 }
 
 @media (min-width: 719px) {
